4f781b0192fbced61f45e8989aff5d73fef7b667
rafaax <rafavfx1@gmail.com>
2024-04-12 11:59:09 -0300

    query first row

--- select.php
+++ select.php
@@ -14,11 +14,15 @@ echo json_encode($array_users) . '<br>';
 
 // query com array 
 $buscar = array(
-    'quantidade' => '60'
+    'quantidade' => 60,
+    'first_row' => 100  
 );
 $quantidade = DB::query('SELECT * from quantidade where quantidade <=  "%i_quantidade" ', $buscar);
 
 // print_r($quantidade);
 foreach($quantidade as $quantidade_value){
-    echo 'ID: ' . $quantidade_value['id'] . ' ---- Quantidade:' . $quantidade_value['quantidade'] . '<BR>';
+    echo 'ID: ' . $quantidade_value['id'] . ' ---- Quantidade:' . $quantidade_value['quantidade'] . '<br>';
 }
+
+$one_row = DB::queryFirstRow('SELECT * from quantidade where quantidade <= %i_first_row', $buscar);
+print_r($one_row);
