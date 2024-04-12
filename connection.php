eda868a80fd4bfa427fac6af575ee14f6777d262
rafaax <rafavfx1@gmail.com>
2024-04-12 11:54:02 -0300

    SELECT PASSANDO ARRAY

--- connection.php
+++ connection.php
@@ -1,9 +1,6 @@
 <?php 
 require 'vendor/autoload.php';
 
-
 DB::$user = 'root';
 DB::$password = '';
 DB::$dbName = 'raphael';
-
-$result = DB::query("SELECT * FROM users");
--- select.php
+++ select.php
@@ -2,8 +2,23 @@
 
 require 'connection.php';
 
+
+// query com string basica
 $users = DB::query("SELECT * from users where nome = %s ", 'raphael' );
 
+$array_users = [];
 foreach($users as $user){
-    echo $user['nome'];
+    $array_users[] = $user['nome'];
+}
+echo json_encode($array_users) . '<br>';
+
+// query com array 
+$buscar = array(
+    'quantidade' => '60'
+);
+$quantidade = DB::query('SELECT * from quantidade where quantidade <=  "%i_quantidade" ', $buscar);
+
+// print_r($quantidade);
+foreach($quantidade as $quantidade_value){
+    echo 'ID: ' . $quantidade_value['id'] . ' ---- Quantidade:' . $quantidade_value['quantidade'] . '<BR>';
 }
