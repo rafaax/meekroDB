bce7e4a46fb81e36f3518e6f686bfce1e8bbcc1b
rafaax <rafavfx1@gmail.com>
2024-04-12 14:05:49 -0300

    queryWalk

--- select.php
+++ select.php
@@ -37,6 +37,14 @@ echo 'Quantidade do Ultimo ID: ' . $first_field  . PHP_EOL;
 list($id, $quantidade) = DB::queryFirstList('select id, quantidade from quantidade order by id desc');
 
 $quantidades = DB::queryFirstColumn('SELECT quantidade from quantidade');
+print_r($quantidades);
+
 
 $full_columns = DB::queryFullColumns('SELECT * from users');
-// print_r($full_columns);
+print_r($full_columns);
+
+
+$Walk = DB::queryWalk("SELECT * FROM users");
+while ($row = $Walk->next()) {
+    print_r($row); // assoc array for one row
+}
